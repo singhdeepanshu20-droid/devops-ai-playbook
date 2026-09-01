90cbe553a4815e3f316292bb8e8dbcceca22e71a
Deepanshu Singh <singhdeepanshu20@gmail.com>
2026-09-01 15:40:53 +1000

    chore: downgrade react-router-dom to v6 and update App test case expectations

--- projects/boutique-microservices/frontend/src/App.test.tsx
+++ projects/boutique-microservices/frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders boutique title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/Luxury Boutique/i);
+  expect(titleElement).toBeInTheDocument();
 });
